ca2cd976af9ed9734d725cd4ba9e7f3ccd974300
tortmann <thomas.ortmann@edu.fh-joanneum.at>
2017-02-02 16:40:01 +0100

    Home/Welcome page updated to fit theme

--- raumterminplaner_frontend/src/app/home/home.component.ts
+++ raumterminplaner_frontend/src/app/home/home.component.ts
@@ -4,12 +4,19 @@ import {Router} from "@angular/router";
 
 @Component({
   template: `
+    <div *ngIf="!givenName" class="jumbotron">
+      <a class="navbar-brand"><img src="../../readme_logo.png" class="logoJumbotron" alt="Logo Jumbotron"/></a>
+      <h1>Willkommen!</h1>
+    </div>    
+    <div *ngIf="givenName" class="jumbotron">
+      <a class="navbar-brand"><img src="../../readme_logo.png" class="logoJumbotron" alt="Logo Jumbotron"/></a>
+      <h1>Willkommen {{givenName}}!</h1>
+    </div>
     <div *ngIf="givenName" class="alert alert-success" role="alert">      
       <span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span>&nbsp;<b>{{ message }}</b>      
-    </div>
-    
-    <div *ngIf="!givenName" class="alert alert-success" role="alert">
-      <button *ngIf="!givenName" class="btn btn-loginLogout" (click)="login()">
+    </div>    
+    <div *ngIf="!givenName" class="alert alert-danger" role="alert">
+      <button *ngIf="!givenName" class="btn btn-custom" (click)="login()">
         <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>
       </button>
       &nbsp;&nbsp;<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>&nbsp;<b>{{ ErrorMessage }}</b>
@@ -23,7 +30,7 @@ export class HomeComponent {
   }
 
   public message: string = 'Sie sind nun eingeloggt!';
-  public ErrorMessage: string = 'Willkommen, bitte loggen Sie sich ein!';
+  public ErrorMessage: string = 'Sie sind derzeit nicht eingeloggt!';
 
   login(): void {
     this.oauthService.initImplicitFlow();
